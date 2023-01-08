3dd79cb938479d0f86ef97e535dec34eaed2c505
arch-spatula <shkim516@naver.com>
2023-01-09 00:00:52 +0900

    feat: 마이페이지 낙관적 업데이트 #1 #2
    마이페이지 낙관적 업데이트 일부만 구현했습니다. WARN 피드백을 해결하지 못했습니다.

--- src/screen/MyPage.jsx
+++ src/screen/MyPage.jsx
@@ -1,10 +1,10 @@
 import styled from '@emotion/native';
-import { Text, TouchableOpacity, View } from 'react-native';
-import { useQuery, useMutation } from 'react-query';
+import { Text, View } from 'react-native';
+import { useQuery, useMutation, useQueryClient } from 'react-query';
 import {
   getDocs,
+  onSnapshot,
   addDoc,
-  deleteDoc,
   query,
   collection,
   orderBy,
@@ -17,7 +17,7 @@ import {
 import { dbService, authService } from '../shared/firebase';
 import { getAuth } from 'firebase/auth';
 import { v4 as uuid } from 'uuid';
-import { COLORS } from '../shared/color';
+import { ManageList } from '../components';
 
 const adminInfo = {
   id: 'catchPillAdmin@email.com',
@@ -46,38 +46,17 @@ const MyPage = () => {
       });
   };
 
-  // TODO: 파이어 베이스 초대 받으면 문서 이름 알아내기
-  // const q = query(collection(dbService, ''), orderBy(''));
-
-  const newPill = {
-    id: uuid(),
-    userId: uid,
-    pillName: '아침약',
-    time: '08:00',
-    isTaken: false,
-  };
-
-  // /pill
-  const addPill = (newPill) => {
-    const selectedDoc = query(collection(dbService, 'pill'));
-    addDoc(selectedDoc, newPill)
-      .then(() => {
-        console.log('추가 성공');
-      })
-      .catch((error) => console.log('error', error.errorMessage));
-  };
-
   // TODO: 쿼리키 통일하기
   // UID를 조회합니다.
   const getUID = () => {
     return getAuth();
   };
+
   const { data: uid } = useQuery('uid', getUID, {
     select: (data) => {
-      return data.currentUser.uid;
+      return data?.currentUser?.uid;
     },
   });
-  console.log('getUid', uid);
 
   // read users pill
   const getUsersPillList = (uid) => {
@@ -96,35 +75,76 @@ const MyPage = () => {
     data: pillList,
   } = useQuery('pill-list', () => getUsersPillList(uid), {
     enabled: !!uid,
-    select: (data) => data.docs.map((doc) => ({ ...doc.data(), id: doc.id })),
+    select: (data) => {
+      return data.docs.map((doc) => {
+        // console.log('doc', doc);
+        return {
+          id: doc.id,
+          userId: doc?.data()?.userId,
+          pillName: doc?.data()?.pillName,
+          time: doc?.data()?.time,
+          isTaken: doc?.data()?.isTaken,
+        };
+      });
+    },
   });
 
-  const renderPillList = pillList?.map((pillListItem) => (
-    <ManageListContainer>
-      <ManageListTitle>{pillListItem.pillName}</ManageListTitle>
-      <ButtonGroupContainer>
-        <TouchableOpacity>
-          <ManageButton buttonColor={COLORS.BLACK}>편집</ManageButton>
-        </TouchableOpacity>
-        <TouchableOpacity>
-          <ManageButton buttonColor={COLORS.DANGER}>삭제</ManageButton>
-        </TouchableOpacity>
-      </ButtonGroupContainer>
-    </ManageListContainer>
+  const queryClient = useQueryClient();
+
+  // 새로운 약 목록을 추가하는 Mutation 함수입니다.
+  const postNewPill = (newPill) => {
+    const selectedDoc = collection(dbService, 'pill');
+    return addDoc(selectedDoc, newPill);
+  };
+
+  // console.log('getUid', uid);
+
+  const newPill = {
+    // id: uuid(),
+    userId: uid,
+    pillName: '점심약',
+    time: '12:00',
+    isTaken: false,
+  };
+
+  const { mutate: addPill } = useMutation(() => postNewPill(newPill), {
+    onMutate: async () => {
+      // 재요청으로 인한 덮어쓰기를 막습니다.
+      await queryClient.cancelQueries('pill-list');
+      const previousPillData = queryClient.getQueryData('pill-list');
+      queryClient.setQueriesData('pill-list', (oldPillData) => {
+        return { ...oldPillData, docs: [...oldPillData?.docs, newPill] };
+      });
+      return { previousPillData };
+    },
+    onError: (_, __, context) => {
+      queryClient.setQueriesData('pill-list', context.previousPillData);
+    },
+    onSuccess: (data) => {
+      console.log('data', data);
+    },
+    onSettled: () => {
+      // 클라이언트 상태가 서버 상태랑 동일하게 싱크를 맞추어줍니다.
+      queryClient.invalidateQueries('pill-list');
+    },
+  });
+
+  const renderPillList = pillList?.map(({ id, pillName }) => (
+    <ManageList key={id} id={id} pillName={pillName} />
   ));
 
   // 에러 핸들링: 통신 실패의 경우 보여주는 화면입니다.
-  if (isError) {
-    return (
-      <MyPageContainer>
-        <PageTitle>나의 약관리</PageTitle>
-        <View>
-          <Text>에러</Text>
-          <Text>{error.errorMessage}</Text>
-        </View>
-      </MyPageContainer>
-    );
-  }
+  // if (isError) {
+  //   return (
+  //     <MyPageContainer>
+  //       <PageTitle>나의 약관리</PageTitle>
+  //       <View>
+  //         <Text>에러</Text>
+  //         <Text>{error.errorMessage}</Text>
+  //       </View>
+  //     </MyPageContainer>
+  //   );
+  // }
 
   // 통신 중
   if (isLoading) {
@@ -142,16 +162,12 @@ const MyPage = () => {
     <MyPageContainer>
       <PageTitle>나의 약관리</PageTitle>
       {/* TODO: flatList 컴포넌트로 리팩토링하기 */}
-      <AddPill onPress={() => console.log('add Pill')}>
-        <Text>새로운 약 추가하기</Text>
-      </AddPill>
       <AddPill onPress={() => handleLogin()}>
-        <Text>임시 회원가입</Text>
+        <Text>임시 로그인</Text>
       </AddPill>
       <AddPill onPress={() => addPill(newPill)}>
         <Text>약추가</Text>
       </AddPill>
-      {/* 약추가 */}
       {renderPillList}
       {/* TODO: 하단 tabs */}
     </MyPageContainer>
@@ -174,35 +190,6 @@ const AddPill = styled.TouchableOpacity`
   margin: 8px 16px;
   height: 80px;
   border-radius: 16px;
-`;
-
-const ManageListContainer = styled.View`
-  background-color: white;
-  margin: 8px 16px;
-  padding: 12px 16px;
-  height: 80px;
-  border-radius: 16px;
-  box-shadow: 0px 0px 8px rgba(202, 202, 202, 0.23);
-  flex-direction: row;
-  justify-content: space-between;
-  align-items: center;
-`;
-
-const ManageListTitle = styled.Text`
-  font-size: 28px;
-`;
-
-const ButtonGroupContainer = styled.View`
-  flex-direction: row;
-  justify-content: end;
-  gap: 16px;
-`;
-
-const ManageButton = styled.Text`
-  font-size: 20px;
-  line-height: 24px;
-  margin: 0 0 0 16px;
-  color: ${(props) => props.buttonColor};
 `;
 
 export default MyPage;
