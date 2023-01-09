cd99465984dd80b08e505b6fa5e4858cf687ce69
arch-spatula <shkim516@naver.com>
2023-01-09 19:59:58 +0900

    feat: useDeletePillData  #1 #2 #4
    약을 삭제하는 함수를 추가합니다.

--- src/Hooks/usePill.js
+++ src/Hooks/usePill.js
@@ -1,6 +1,6 @@
 import { useMutation } from 'react-query';
 import { dbService } from '../shared/firebase';
-import { addDoc, collection } from 'firebase/firestore';
+import { addDoc, deleteDoc, collection, query, doc } from 'firebase/firestore';
 
 // 약 추가 함수 / firestore에 약 새로운 약 정보 추가
 const addPill = (pill) => {
@@ -10,4 +10,21 @@ const addPill = (pill) => {
 // 약 추가 함수
 export const useAddPillData = () => {
   return useMutation(addPill);
+};
+
+// 약 삭제 함수 / firestore에 새로운 약 정보 삭제
+const deletePill = (pill) => {
+  return deleteDoc(doc(dbService, 'pill', pill));
+};
+
+// 약 삭제 함수
+export const useDeletePillData = () => {
+  return useMutation(deletePill, {
+    onError: () => {
+      console.log('ㅗ');
+    },
+    onSuccess: () => {
+      console.log('ㅇㅇ');
+    },
+  });
 };
