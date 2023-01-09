792abed30807d5c3cc6b52773f663d89e57ee7ea
songjihyun323 <88391843+songjihyun-dev@users.noreply.github.com>
2023-01-09 12:51:25 +0900

    refactor: useQuery 추가 #1 #4 useUID 커스텀 훅 생성

--- src/Hooks/useAuth.js
+++ src/Hooks/useAuth.js
@@ -6,14 +6,26 @@ s;
 export const handleSignUp = async (email, password) => {
   Keyboard.dismiss(); // 버튼 클릭 시 키보드 접기
   return createUserWithEmailAndPassword(auth, email, password);
-  // .then()
-  // .catch((error) => {
-  //   if (error.message.includes('email-already-in-use')) {
-  //     alert(
-  //       '이미 가입된 이메일입니다. 로그인을 시도하거나 다른 이메일을 사용해 주세요.',
-  //     );
-  //   }
-  //   console.log(error);
-  // });
 };
-useMutation(() => handleSignUp(email, password));
+
+useMutation(() => handleSignUp(email, password), {
+  onError: (error) => {
+    if (error.message.includes('email-already-in-use')) {
+      alert(
+        '이미 가입된 이메일입니다. 로그인을 시도하거나 다른 이메일을 사용해 주세요.',
+      );
+    }
+  },
+});
+
+const getUID = () => {
+  return auth;
+};
+
+export const useUID = () => {
+  return useQuery('uid', getUID, {
+    select: (data) => {
+      return data?.currentUser?.uid;
+    },
+  });
+};
