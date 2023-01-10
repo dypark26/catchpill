015ea7684eceecabc97adc9f739378fbf8a91d6c
arch-spatula <shkim516@naver.com>
2023-01-10 17:42:12 +0900

    feat: useGetPillData 낙관적 업데이트 #1 #14

--- src/Hooks/usePill.js
+++ src/Hooks/usePill.js
@@ -1,4 +1,4 @@
-import { useQuery, useMutation } from 'react-query';
+import { useQuery, useMutation, useQueryClient } from 'react-query';
 import { dbService } from '../shared/firebase';
 import {
   addDoc,
@@ -54,14 +54,57 @@ const toggleTakenPill = ({ id, togglePayload }) => {
   });
 };
 
-// 약 isTaken 업데이트 함수
+/**
+ * 약 isTaken 업데이트 함수
+ * 낙관적 업데이트로 캐시를 먼저 업데이트가 되고 서버에 반영되는 패턴입니다.
+ * 참고로 캐시가 업데이트 되면서 UI가 업데이트 됩니다.
+ * @returns useMutation
+ */
 export const useToggleTakenPill = () => {
+  const queryClient = useQueryClient();
   return useMutation(toggleTakenPill, {
-    onSuccess: () => {
-      console.log('성공');
+    onMutate: async (togglePill) => {
+      // 쿼리를 취소합니다.
+      await queryClient.cancelQueries(['pill-list']);
+
+      // 기본 데이터를 저장합니다.
+      const previousPillList = queryClient.getQueryData(['pill-list']);
+
+      // 새롭게 변형된 데이터로 설정합니다.
+      queryClient.setQueriesData(['pill-list'], (old) => {
+        // useGetPillData에서 데이터를 선택하는 것과 유사한 파이어베이한 로직 문제입니다
+        old.docs.map((doc) => {
+          return doc.id === togglePill.id
+            ? // 복용하면서 isTaken을 false에서 true로 변경합니다. 반대도 동작합니다.
+              {
+                ...doc.data(),
+                id: doc.id,
+                isTaken: togglePill.togglePayload.isTaken,
+              }
+            : // 안 건드린 약입니다.
+              { ...doc.data(), id: doc.id };
+        });
+
+        // 파이어베이스로 선택한 데이터를 반환합니다.
+        return old;
+      });
+
+      // 저장한 값을 먼저 반환합니다.
+      return { previousPillList };
     },
-    onError: () => {
-      console.log('실패');
+
+    onSuccess: () => {
+      // console.log('성공');
+    },
+
+    onError: (__err, __togglePill, context) => {
+      // console.log('실패');
+      queryClient.setQueryData(['pill-list'], context.previousPillList);
+    },
+
+    onSettled: (togglePill) => {
+      // 통신 후 서버에 반영합니다.
+      queryClient.invalidateQueries(['pill-list']);
     },
   });
 };
