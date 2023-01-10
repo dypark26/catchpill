e3698529f9832b8f47d20d4b73bc1870435c49ba
arch-spatula <shkim516@naver.com>
2023-01-10 15:31:54 +0900

    feat: 복용여부 toggle 기능 추가 #1 #14
    UI를 반영했습니다. toggle 업데이트 통신까지 동작합니다.
    낙관적 업데이트는 아직 없습니다.

---
+++ src/components/ToggleList.jsx
@@ -0,0 +1,48 @@
+import { COLORS } from '../shared/color';
+import styled from '@emotion/native';
+import ToggleButton from './ToggleButton';
+
+// TODO: 조건부 스타일링 isTaken값이 false랑 true에 따라 다른 UI가 보입니다.
+const ToggleList = ({ pillName, time, id, isTaken, uid }) => {
+  const togglePayload = { pillName, time, isTaken, uid };
+
+  return (
+    <ToggleListItem isTaken={isTaken}>
+      <ToggleListItemTextContainer>
+        <ToggleListItemTitle>{pillName}</ToggleListItemTitle>
+        <ToggleListItemTime>{time}</ToggleListItemTime>
+      </ToggleListItemTextContainer>
+      <ToggleButton id={id} togglePayload={togglePayload} />
+    </ToggleListItem>
+  );
+};
+
+const ToggleListItem = styled.View`
+  height: 80px;
+  padding: 12px 16px;
+  box-shadow: 0px 0px 8px rgba(202, 202, 202, 0.23);
+  background-color: ${(props) =>
+    props.isTaken ? COLORS.POINT_COLOR_100 : 'white'};
+  margin: 8px 16px;
+  border-radius: 16px;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+`;
+
+const ToggleListItemTextContainer = styled.View`
+  flex-direction: row;
+  align-items: center;
+`;
+
+const ToggleListItemTitle = styled.Text`
+  font-size: 28px;
+  text-overflow: ellipsis;
+`;
+
+const ToggleListItemTime = styled.Text`
+  font-size: 16px;
+  margin: 0 0 0 16px;
+`;
+
+export default ToggleList;
