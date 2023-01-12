5c771e10e5ea5e605dcae2184db44bf1eb12ecc0
hobak12 <tjsgud2564@naver.com>
2023-01-13 01:25:17 +0900

    fix: 리스트 박스 텍스트 정렬 수정 #1 by @hobak12

--- src/components/BoxShadow.jsx
+++ src/components/BoxShadow.jsx
@@ -7,6 +7,7 @@ const BoxShadow = ({ children, color }) => {
       style={{
         ...Platform.select({
           ios: {
+            justifyContent: 'center',
             height: 80,
             paddingHorizontal: 16,
             paddingVertical: 12,
@@ -24,10 +25,10 @@ const BoxShadow = ({ children, color }) => {
             shadowRadius: 4.65,
           },
           android: {
+            justifyContent: 'center',
             height: 80,
             paddingHorizontal: 16,
             paddingVertical: 12,
-
             marginHorizontal: 16,
             marginVertical: 8,
             borderRadius: 16,
